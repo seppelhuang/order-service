975bdb60975444bb289188513a5def37b10a6190
huangsp <seppelhuang@163.com>
2019-04-14 21:54:33 +0800

    git init

---
+++ src/main/java/cn/seppel/controller/TestEndPointController.java
@@ -0,0 +1,42 @@
+package cn.seppel.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@Slf4j
+public class TestEndPointController {
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id) {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "product id : " + id;
+    }
+
+    @GetMapping("/order/{id}")
+    public String getOrder(@PathVariable String id) {
+       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("order id : " + id);
+        return "order id : " + id;
+    }
+
+    @GetMapping("/getPrinciple")
+    public OAuth2Authentication getPrinciple(OAuth2Authentication oAuth2Authentication, Principal principal, Authentication authentication) {
+        log.info(oAuth2Authentication.getUserAuthentication().getAuthorities().toString());
+        log.info(oAuth2Authentication.toString());
+        log.info("principal.toString() " + principal.toString());
+        log.info("principal.getName() " + principal.getName());
+        log.info("authentication: " + authentication.getAuthorities().toString());
+
+        return oAuth2Authentication;
+    }
+}
